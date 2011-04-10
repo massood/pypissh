e8c8d3036892afb4817413c1d27646dd3edda27f
Martin v. L?wis <martin@v.loewis.de>
2011-04-10 09:48:15 +0200

    Issue #1: Consider --root, by using the install_lib directory as a target for the pth file.

--- setup.py
+++ setup.py
@@ -6,12 +6,8 @@ from distutils.command.install_data import install_data
 class install_sitepackages(install_data):
     def finalize_options(self):
         self.warn_dir = 0
-        if os.sep == '/':
-            self.install_dir = os.path.join(sys.prefix, "lib", 
-                                            "python"+sys.version[:3],
-                                            "site-packages")
-        else:
-            self.install_dir = os.path.join(sys.prefix, "lib", "site-packages")
+        self.install_dir = self.get_finalized_command('install').install_lib
+        install_data.finalize_options(self)
 
 setup(name='pypissh',
       version='1.1',
